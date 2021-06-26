54c74b65a60324aeef9813ff61f6737c91af61a2
maximilianotortosa@gmail.com <maximilianotortosa@gmail.com>
2021-06-26 20:11:51 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from './Components/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
 	return (
 		<>
 			<Router>
 				<Header />
+				<Footer />
 			</Router>
 		</>
 	);
